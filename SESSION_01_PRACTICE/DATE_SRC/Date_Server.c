b99d0124b566169710b5ff2fa170f981f5750125
kamleshdugade <kamleshdugade777@gmail.com>
2022-12-17 23:02:53 +0530

    Is_Valid_date() implemented!

--- SESSION_01_PRACTICE/DATE_SRC/Date_Server.c
+++ SESSION_01_PRACTICE/DATE_SRC/Date_Server.c
@@ -12,6 +12,7 @@ struct Date* create_date()
 
 void init_date(struct Date* p_date, int dd, int mm, int yy)
 {
+    assert(is_validate_date(dd, mm, yy)); 
     p_date->Day = dd;
     p_date->Month = mm;
     p_date->Year = yy;
@@ -34,18 +35,21 @@ int get_year(struct Date* p_date)
 
 int set_day(struct Date* p_date, int new_day)
 {
+    assert(is_validate_date(new_day, p_date->Month, p_date->Year));
     p_date->Day = new_day;
     return TRUE;
 }
 
 int set_month(struct Date* p_date, int new_month)
 {
+    assert(is_validate_date(p_date->Day, new_month, p_date->Year));
     p_date->Month = new_month;
     return TRUE;
 }
 
 int set_year(struct Date* p_date, int new_year)
 {
+    assert(is_validate_date(p_date->Day, p_date->Month, new_year));
     p_date->Year = new_year;
     return TRUE;
 }
@@ -63,12 +67,28 @@ void destroy_date(struct Date* p_date)
 }
 
 /* Not to be use by client directly, Only for internal Validation*/
-static int validate_date(int dd, int mm, int yy)
+static int is_validate_date(int day, int month, int year)
 {
-    if((dd <= 31) && (mm <= 12) && (yy > 0))
-    {
-        puts("\n****** DATE VALIDATED SUCCESSFULLY ******\n");
-        return TRUE;
-    }
-    return FALSE;
+    if(1000 <= year <= 3000)
+       {
+         if((month==1 || month==3 || month==5|| month==7|| month==8||month==10||month==12) && day>0 && day<=31)
+            return TRUE;
+         else 
+            if(month==4 || month==6 || month==9|| month==11 && day>0 && day<=30)
+                    return TRUE;
+                else
+                    if(month==2)
+                    {
+                        if((year%400==0 || (year%100!=0 && year%4==0)) && day>0 && day<=29)
+                            return TRUE;
+                        else if(day>0 && day<=28)
+                            return TRUE;
+                        else
+                            return TRUE;
+                    }
+                    else
+                        return FALSE;
+        }
+    else
+        return FALSE;
 }
--- SESSION_02_PRACTICE/DATE_CPP/Date.cpp
+++ SESSION_02_PRACTICE/DATE_CPP/Date.cpp
@@ -55,7 +55,28 @@ void Date::show() const
     std::cout<< day << "-" << month << "-" << year << std::endl;
 }
 
-bool Date::is_date_valid(int init_day, int init_month, int init_year )
+bool Date::is_date_valid(int day, int month, int year )
 {
-    return(TRUE);
+    if(1000 <= year <= 2023)
+       {
+         if((month==1 || month==3 || month==5|| month==7|| month==8||month==10||month==12) && day>0 && day<=31)
+            return TRUE;
+         else 
+            if(month==4 || month==6 || month==9|| month==11 && day>0 && day<=30)
+                    return TRUE;
+                else
+                    if(month==2)
+                    {
+                        if((year%400==0 || (year%100!=0 && year%4==0)) && day>0 && day<=29)
+                            return TRUE;
+                        else if(day>0 && day<=28)
+                            return TRUE;
+                        else
+                            return TRUE;
+                    }
+                    else
+                        return FALSE;
+        }
+    else
+        return FALSE;
 }
